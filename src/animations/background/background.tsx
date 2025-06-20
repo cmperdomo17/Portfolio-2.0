583dac2331899cf507add780a56d89080a90e51e
cmperdomo17 <carlosmarioperdomo555@gmail.com>
2025-06-20 13:52:41 -0500

    feat: update styles

--- src/animations/background/background.tsx
+++ src/animations/background/background.tsx
@@ -2,9 +2,9 @@ import Particles from "@/animations/particles/particles.jsx";
 
 export default function AnimatedBackground() {
   return (
-    <div className="absolute h-full w-full inset-0 -z-10">
+    <div className="absolute h-full w-full inset-0 z-[-1]">
       <Particles
-        particleColors={["#02C3BD", "#02C3BD"]}
+        particleColors={["#fff"]}
         className=""
         particleCount={200}
         particleSpread={12}
